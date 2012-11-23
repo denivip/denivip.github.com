3fe0b4ef764d4d6570654bc665c11336e3b0423e
Vladimir Semenyuk <vladimir.semenyuk@gmail.com>
2012-11-23 14:55:09 +0400

    page update

--- js/index.js
+++ js/index.js
@@ -1,4 +1,47 @@
 $(function() {
+    $('.twitter').click(function(e) {
+        e.stopImmediatePropagation();
+        e.preventDefault();
+
+        var width  = 575,
+            height = 400,
+            left   = ($(window).width()  - width)  / 2,
+            top    = ($(window).height() - height) / 2,
+            url    = this.href +
+                '?hashtags='+$(e.target).parent().data('hashtags') +
+                '&text='   + $(e.target).parent().data('text') +
+                '&url='     + $(e.target).parent().data('url')
+            ,
+            opts   = 'status=1' +
+                ',width='  + width  +
+                ',height=' + height +
+                ',top='    + top    +
+                ',left='   + left;
+
+
+        window.open(url, 'Twitter', opts);
+
+    });
+
+    $('.google').click(function(e){
+        e.stopImmediatePropagation();
+        e.preventDefault();
+
+        var width  = 480,
+            height = 380,
+            left   = ($(window).width()  - width)  / 2,
+            top    = ($(window).height() - height) / 2,
+            opts   = 'status=1' +
+            ',width='  + width  +
+            ',height=' + height +
+            ',top='    + top    +
+            ',left='   + left;
+
+        window.open('https://plus.google.com/share?url=' + $(e.target).parent().data('url') +
+            '&title=' + $(e.target).parent().data('url'), 'Google+',opts);
+    });
+
+
     var animateThread = null,
         flask = $('#flask');
 
